a452de3372554fb4a0d5624962287edee302e928
Ben Guthrie <ben-guthrie@pluralsight.com>
2022-03-23 20:50:51 -0600

    add axios to client and cors to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 
 import apiRouter from './routers'
@@ -6,6 +7,7 @@ const SERVER_PORT = 3001
 
 const app = express()
 
+app.use(cors({ origin: '*' }))
 app.use('/api', apiRouter)
 
 app.listen(SERVER_PORT, () => {
